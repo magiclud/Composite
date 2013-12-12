4709ed77a9a7c2b528f5adc2a25209d55917d416
Aga <agnieszkalud@op.pl>
2013-12-12 16:05:13 +0100

    Composite

---
+++ src/main/java/eu/jpereira/trainings/designpatterns/structural/composite/model/CompositeShape.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2011 Joao Miguel Pereira
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package eu.jpereira.trainings.designpatterns.structural.composite.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Joao Pereira
+ * 
+ */
+public abstract class CompositeShape extends Shape {
+
+	List<Shape> shapes;
+
+	public CompositeShape() {
+		this.shapes = createShapesList();
+	}
+
+	/**
+	 * Remove a shape from this shape childrens
+	 * 
+	 * @param shape
+	 *            the shape to remove
+	 * @return true if the shape was present and was removed, false if the shape
+	 *         was not present
+	 */
+	public boolean removeShape(Shape shape) {
+		// TODO: implement moje
+		return shapes.remove(shape);
+	}
+
+	/**
+	 * Return the total shapes count in case this is a composite
+	 * 
+	 * @return the total count of shapes if the shape is composite. -1 otherwise
+	 */
+	public int getShapeCount() {
+		// TODO: implement moje
+		return shapes.size();
+
+	}
+
+	/**
+	 * Add a shape to this shape.
+	 * 
+	 * @param shape
+	 *            The shape to add
+	 * @throws ShapeDoesNotSupportChildren
+	 *             if this shape is not a composite
+	 */
+	public void addShape(Shape shape) {
+		// TODO: Implement moje
+		shapes.add(shape);
+	}
+
+	public List<Shape> getShapes() {
+		// TODO: Implement moje
+		return shapes;
+
+	}
+
+	/**
+	 * @param circle
+	 * @return
+	 */
+	public List<Shape> getShapesByType(ShapeType circle) {
+		List<Shape> newShapes = new ArrayList();
+		for (Shape temp : shapes) {
+			if (temp.getType().equals(circle)) {
+				newShapes.add(temp);
+			}
+		}
+		return newShapes;
+		// TODO: Implement takie cos wymyslilam
+	}
+
+	/**
+	 * Return all shapes that are leafs in the tree
+	 * 
+	 * @return
+	 */
+	public List<Shape> getLeafShapes() {
+		// TODO: Implement moje
+		return createShapesList();
+	}
+
+	/**
+	 * Factory method for the list of children of this shape
+	 * 
+	 * @return
+	 */
+	protected List<Shape> createShapesList() {
+		shapes = new ArrayList();
+		return shapes;
+		// TODO: Implement
+		// TODO nie wiem
+	}
+}
